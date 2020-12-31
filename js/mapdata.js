98fa8b4ff72cc7571350257006387d98f2f0f83f
jtgarlits <jtgarlits@gmail.com>
2020-12-31 02:42:19 -0500

    first commit

---
+++ js/mapdata.js
@@ -0,0 +1,503 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "650", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    
+    //Label defaults
+    label_color: "white",
+    hide_labels: "no",
+    border_color: "white",
+    
+    //State defaults
+    state_description: "",
+    state_color: "#86C232",
+    state_hover_color: "#61892F",
+    state_url: "",
+    all_states_inactive: "no",
+    
+    //Location defaults
+    location_description: "Field Office",
+    location_color: "#ff46a2",
+    location_pulse: "yes",
+    location_pulse_size: "3",
+    location_opacity: ".8",
+    location_url: "http://simplemaps.com",
+    all_states_zoomable: "yes",
+    location_size: "40",
+    location_type: "marker",
+    all_locations_inactive: "no",
+    url_new_tab: "yes",
+    auto_load: "yes",
+   
+    //Zoom settings
+    zoom: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    
+    //Advanced settings
+    div: "map",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    label_hover_color: "",
+    label_size: "",
+    label_font: "",
+    popups: "detect"
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      hide: "no",
+      inactive: "no",
+      population: 1407006,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    AK: {
+      name: "Alaska",
+      hide: "no",
+      inactive: "no",
+      population: 3030522,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    FL: {
+      name: "Florida",
+      inactive: "no",
+      hide: "no",
+      population: 21733312,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    NH: {
+      name: "New Hampshire",
+      hide: "no",
+      inactive: "no",
+      population: 1366275,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    VT: {
+      name: "Vermont",
+      hide: "no",
+      inactive: "no",
+      population: 623347,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    ME: {
+      name: "Maine",
+      hide: "no",
+      inactive: "no",
+      population: 1350141,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    RI: {
+      name: "Rhode Island",
+      hide: "no",
+      inactive: "no",
+      population: 1057125,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    NY: {
+      name: "New York",
+      hide: "no",
+      inactive: "no",
+      population: 19336776,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    PA: {
+      name: "Pennsylvania",
+      hide: "no",
+      inactive: "no",
+      population: 12783254,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    NJ: {
+      name: "New Jersey",
+      hide: "no",
+      inactive: "no",
+      population: 8882371,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    DE: {
+      name: "Delaware",
+      hide: "no",
+      inactive: "no",
+      population: 986809,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    MD: {
+      name: "Maryland",
+      hide: "no",
+      inactive: "no",
+      population: 6055802,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    VA: {
+      name: "Virginia",
+      hide: "no",
+      inactive: "no",
+      population: 8590563,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    WV: {
+      name: "West Virginia",
+      hide: "no",
+      inactive: "no",
+      population: 1784787,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    OH: {
+      name: "Ohio",
+      hide: "no",
+      inactive: "no",
+      population: 11693217,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    IN: {
+      name: "Indiana",
+      hide: "no",
+      inactive: "no",
+      population: 6754953,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    IL: {
+      name: "Illinois",
+      hide: "no",
+      inactive: "no",
+      population: 12587530,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    CT: {
+      name: "Connecticut",
+      hide: "no",
+      inactive: "no",
+      population: 3557006,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    WI: {
+      name: "Wisconsin",
+      hide: "no",
+      inactive: "no",
+      population: 5832655,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    NC: {
+      name: "North Carolina",
+      hide: "no",
+      inactive: "no",
+      population: 10600823,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    DC: {
+      name: "District of Columbia",
+      hide: "no",
+      inactive: "no",
+      population: 712816,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    MA: {
+      name: "Massachusetts",
+      hide: "no",
+      inactive: "no",
+      population: 6893574,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    TN: {
+      name: "Tennessee",
+      hide: "no",
+      inactive: "no",
+      population: 6886834,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    AR: {
+      name: "Arkansas",
+      hide: "no",
+      inactive: "no",
+      population: 3030522,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    MO: {
+      name: "Missouri",
+      hide: "no",
+      inactive: "no",
+      population: 6151548,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    GA: {
+      name: "Georgia",
+      hide: "no",
+      inactive: "no",
+      population: 10710017,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    SC: {
+      name: "South Carolina",
+      hide: "no",
+      inactive: "no",
+      population: 5218040,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    KY: {
+      name: "Kentucky",
+      hide: "no",
+      inactive: "no",
+      population: 4477251,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    AL: {
+      name: "Alabama",
+      hide: "no",
+      inactive: "no",
+      population: 4921532,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    LA: {
+      name: "Louisiana",
+      hide: "no",
+      inactive: "no",
+      population: 4645318,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    MS: {
+      name: "Mississippi",
+      hide: "no",
+      inactive: "no",
+      population: 2966786,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    IA: {
+      name: "Iowa",
+      hide: "no",
+      inactive: "no",
+      population: 3163561,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    MN: {
+      name: "Minnesota",
+      hide: "no",
+      inactive: "no",
+      population: 5657342,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    OK: {
+      name: "Oklahoma",
+      hide: "no",
+      inactive: "no",
+      population: 3980783,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    TX: {
+      name: "Texas",
+      hide: "no",
+      inactive: "no",
+      population: 29360759,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    NM: {
+      name: "New Mexico",
+      hide: "no",
+      inactive: "no",
+      population: 2106319,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    KS: {
+      name: "Kansas",
+      hide: "no",
+      inactive: "no",
+      population: 2913805,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    NE: {
+      name: "Nebraska",
+      hide: "no",
+      inactive: "no",
+      population: 1937552,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    SD: {
+      name: "South Dakota",
+      hide: "no",
+      inactive: "no",
+      population: 892717,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    ND: {
+      name: "North Dakota",
+      hide: "no",
+      inactive: "no",
+      population: 765309,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    WY: {
+      name: "Wyoming",
+      hide: "no",
+      inactive: "no",
+      population: 582328,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    MT: {
+      name: "Montana",
+      hide: "no",
+      inactive: "no",
+      population: 1080577,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    CO: {
+      name: "Colorado",
+      hide: "no",
+      inactive: "no",
+      population: 5807719,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    UT: {
+      name: "Utah",
+      hide: "no",
+      inactive: "no",
+      population: 3249879,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    AZ: {
+      name: "Arizona",
+      hide: "no",
+      inactive: "no",
+      population: 7421401,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    NV: {
+      name: "Nevada",
+      hide: "no",
+      inactive: "no",
+      population: 3138259,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    OR: {
+      name: "Oregon",
+      hide: "no",
+      inactive: "no",
+      population: 4241507,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    WA: {
+      name: "Washington",
+      hide: "no",
+      inactive: "no",
+      population: 7693612,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    CA: {
+      name: "California",
+      hide: "no",
+      inactive: "no",
+      population: 39368078,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    MI: {
+      name: "Michigan",
+      hide: "no",
+      inactive: "no",
+      population: 9966555,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    ID: {
+      name: "Idaho",
+      hide: "no",
+      inactive: "no",
+      population: 1826913,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes",
+      inactive: "no",
+      population: 168485,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes",
+      inactive: "no",
+      population: 106235,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes",
+      inactive: "no",
+      population: 3189068,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      inactive: "no",
+      population: 51433,
+      color: "#86C232",
+      hover_color: "#61892F"
+    },
+    AS: {
+      name: "American Samoa",
+      hide: "yes",
+      inactive: "no",
+      population: 49437,
+      color: "#86C232",
+      hover_color: "#61892F"
+    }
+  },
+  locations: {},
+  regions: {},
+  labels: {}
+};
+
---
+++ js/script.js
@@ -0,0 +1,149 @@
+const us_url = 'https://api.covidtracking.com/v1/us/current.json';
+const state_url = 'https://api.covidtracking.com/v1/states/current.json';
+const uspop = 328200000;
+
+const statemap = simplemaps_usmap_mapdata.state_specific;
+
+let maploc = document.getElementById('maptemp');
+
+let datatitle = document.getElementById('datatitle');
+
+let dispcases = document.getElementById('cases');
+let dispper100k = document.getElementById('per100k');
+let dispdeaths = document.getElementById('deaths');
+let dispcaseinc = document.getElementById('caseinc');
+let dispdeathinc = document.getElementById('deathinc');
+
+let us;
+let states;
+
+let uscases;
+let uspercap;
+let usdeaths;
+let uscaseinc;
+let usdeathinc;
+
+let cases;
+let percap;
+let deaths;
+
+let statespercap = {};
+
+fetch(us_url)
+    .then(usObj => usObj.json())
+    .then(usData => setUS(usData));
+
+fetch(state_url)
+    .then(stateObj => stateObj.json())
+    .then(stateData => setState(stateData));
+
+$("#map").on("click", checkClick);
+
+// Funtions
+
+function setUS(json) {
+
+    us = json;
+    console.log(us);
+
+    uscases = json[0].positive;
+    uspercap = (uscases / uspop) * 100000;
+    usdeaths = json[0].death;
+    uscaseinc = json[0].positiveIncrease;
+    usdeathinc = json[0].deathIncrease;
+
+    dispcases.innerText = uscases.toLocaleString('en');
+    dispper100k.innerText = uspercap.toLocaleString('en', {maximumFractionDigits: 0});
+    dispdeaths.innerText = usdeaths.toLocaleString('en');
+    dispcaseinc.innerText = uscaseinc.toLocaleString('en');
+    dispdeathinc.innerText = usdeathinc.toLocaleString('en');
+
+}
+
+function setState(json) {
+
+    states = json;
+    console.log(states);
+    console.log(statemap);
+
+    for (let st of states){
+
+        let id = st.state;
+        let pop = statemap[id].population;
+
+        let temp = ((st.positive) / (pop) * 100000 );
+
+        statespercap[id] = temp;
+
+        switch (true) {
+            case (temp > 7500):
+                simplemaps_usmap_mapdata.state_specific[id].color = '#ff4848';
+                simplemaps_usmap_mapdata.state_specific[id].hover_color = '#921d1d';
+                break;
+            case (temp > 5000):
+                simplemaps_usmap_mapdata.state_specific[id].color = ' #e4e000';
+                simplemaps_usmap_mapdata.state_specific[id].hover_color = '#a09e15';
+                break;
+        }
+
+    }
+
+    loadmap();
+
+}
+
+function checkClick(e){
+
+    let check = ($(e.target))[0].classList[0];
+    let type = ($(e.target))[0].localName;
+
+    let sel =  $(e.target).text();
+
+    if(sel == "" && check == undefined && type == "path"){
+        dispcases.innerText = uscases.toLocaleString('en');
+        dispper100k.innerText = uspercap.toLocaleString('en', {maximumFractionDigits: 0});
+        dispdeaths.innerText = usdeaths.toLocaleString('en');
+        dispcaseinc.innerText = uscaseinc.toLocaleString('en');
+        dispdeathinc.innerText = usdeathinc.toLocaleString('en');
+        datatitle.innerText = 'United States';
+    }
+    else if (sel == "" && check != undefined){
+        sel = check.slice(-2);
+        let temp = states.filter(obj => {
+            return obj.state == sel
+            
+        })
+        dispcases.innerText = temp[0].positive.toLocaleString('en');
+        dispper100k.innerText = statespercap[sel].toLocaleString('en', {maximumFractionDigits: 0});
+        dispdeaths.innerText = temp[0].death.toLocaleString('en');
+        dispcaseinc.innerText =temp[0].positiveIncrease.toLocaleString('en');
+        dispdeathinc.innerText =temp[0].deathIncrease.toLocaleString('en');
+        datatitle.innerText = statemap[sel].name;
+
+    }
+    else if (sel != ""){
+        let temp = states.filter(obj => {
+            return obj.state == sel
+            
+        })
+        dispcases.innerText = temp[0].positive.toLocaleString('en');
+        dispper100k.innerText = statespercap[sel].toLocaleString('en', {maximumFractionDigits: 0});
+        dispdeaths.innerText = temp[0].death.toLocaleString('en');
+        dispcaseinc.innerText =temp[0].positiveIncrease.toLocaleString('en');
+        dispdeathinc.innerText =temp[0].deathIncrease.toLocaleString('en');
+        datatitle.innerText = statemap[sel].name;
+    }
+    else{}
+
+
+}
+
+function loadmap(){
+
+    var head= document.getElementsByTagName('head')[0];
+      var script= document.createElement('script');
+      script.src= 'js/usmap.js';
+      head.appendChild(script);
+
+}
+
